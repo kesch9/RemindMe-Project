77af2140833d0d5da86526f9bc8afe49bb54a93b
Kesch9 <kashenkovsergej@yandex.ru>
2016-01-12 20:45:29 +0300

    Create base project structure

---
+++ app/src/main/java/com/example/kesch/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kesch.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kesch on 12.01.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
